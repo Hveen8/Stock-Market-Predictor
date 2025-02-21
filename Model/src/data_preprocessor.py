ddc2e24d014fb6b14996e4ac74289f807f403a69
DanielOliyarnik <daniel.oliyarnik@gmail.com>
2025-02-21 05:36:21 +0000

    input layer in DataProc

--- Model/src/data_preprocessor.py
+++ Model/src/data_preprocessor.py
@@ -54,3 +54,8 @@ class DataPreprocessor:
         # Removing any odd data depending on batch size
         trim_size = len(dataX) - (len(dataX) % batch_size)
         return dataX[:trim_size], dataY[:trim_size]
+
+    def create_input_layer(self, batch_size, look_back):
+        # store the generated input layer into the class
+        self.input_layer = InputLayer(batch_input_shape=(batch_size, look_back, 1))
+        return self.input_layer
